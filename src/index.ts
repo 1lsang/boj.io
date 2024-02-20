3ce55d74ca0463fd8b2b9b1e9bcdd549cc9c3730
1lsang <ilsangpark.98@gmail.com>
2024-02-20 13:17:24 +0900

    ✨ add command for test

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('boj.io')
+  .description('Baekjoon Online Judge 문제 풀이 검증 자동화 CLI')
+  .version('0.1.0');
+
+program
+  .command('test')
+  .description('주어진 테스트에 대해 코드를 실행하고 결과를 확인합니다.')
+  .argument('<path>', '실행할 파일 경로')
+  .option('-pn, --problem-number <number>', '문제 번호');
+
+program.parse();
